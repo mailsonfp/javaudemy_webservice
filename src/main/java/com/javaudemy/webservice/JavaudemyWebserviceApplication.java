fee7d7f77966847969b4ae377c655fead00fbba2
Mailson Fernando Pereira <mailsonfp.dev@gmail.com>
2020-08-24 23:28:15 -0300

    Criando repositório e projeto

---
+++ src/main/java/com/javaudemy/webservice/JavaudemyWebserviceApplication.java
@@ -0,0 +1,13 @@
+package com.javaudemy.webservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaudemyWebserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaudemyWebserviceApplication.class, args);
+	}
+
+}
